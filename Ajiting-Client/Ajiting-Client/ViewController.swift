15a87f0fe514996b54b482e160cdc764a97ee298
Tsubasa-Kajinuma <Tsubasa_Kajinuma@voyagegroup.com>
2015-02-02 21:40:08 +0900

    create project

---
+++ Ajiting-Client/Ajiting-Client/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ajiting-Client
+//
+//  Created by 梶沼 翼 on 2015/02/02.
+//  Copyright (c) 2015年 Voyage Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Ajiting-Client/Ajiting-ClientTests/Ajiting_ClientTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ajiting_ClientTests.swift
+//  Ajiting-ClientTests
+//
+//  Created by 梶沼 翼 on 2015/02/02.
+//  Copyright (c) 2015年 Voyage Group. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Ajiting_ClientTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
